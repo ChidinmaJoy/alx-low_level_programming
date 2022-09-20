f2fb939b7bb4be4e13889b5d320f27633b7a8c27
ChidinmaJoy <chidinmajoyjoy@gmail.com>
2022-09-19 19:19:46 -0700

    Reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98  - function takes a pointer to an int parameter.
+ * @n: is an integer pointer
+ * Return 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	 *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+ -function that swaps the values of two integers.
+ -function that returns the length of a string.
+ -function that prints a string, followed by a new line, to stdout etc.
