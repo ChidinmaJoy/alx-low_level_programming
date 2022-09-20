74674b887170654d94194114e3fb17b3616db9a8
ChidinmaJoy <chidinmajoyjoy@gmail.com>
2022-09-20 08:53:23 -0700

    Return the lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - prints the lenght of a string
+ * @s: string varialbe
+ * Retun: lenght of the string
+ */
+int_strlen(char *s)
+{
+return (strlen(s));
+}
