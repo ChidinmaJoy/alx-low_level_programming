1fd5d95e123eaddd3e30d7711f788093670c8519
ChidinmaJoy <chidinmajoyjoy@gmail.com>
2022-09-27 07:37:17 -0700

    Memcopy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _memset - fills the fist n bytes of the memory area
- * @s: memory area to be filled
- * @b: constant byte that will fill the first memory area
- * @n: This is the bytes to filled
- * Returns: the pointer to (s)
+ * _memset - fills the memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte that will fill the memory area.
+ * @n: This is the bytes to be filled.
+ * Returns: the pointer to the memory.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	*(s + i) = b;
+		*(s + i) = b;
 
 	return (s);
 }
